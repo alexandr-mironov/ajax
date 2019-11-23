3ccce837799ca0417148a61e507b1e20d0c0f766
Alexandr Mironov <mironov.a.s@outlook.com>
2019-11-23 23:49:20 +0400

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,49 +1,65 @@
-var ajax=function(url,method,data,callback){
-	var xhr = new XMLHttpRequest(),
-		f=(data instanceof FormData)?data:new FormData(),
-		data=data || [],
-		method=method.toUpperCase() ||'GET',
-		url=url||'';
-
-	if(method=='POST'){
-		if(Object.keys(data).length>0 && data instanceof Object && !(data instanceof FormData)){
+var ajax=function(obj){
+	if(obj.url==void 0){
+		console.err('Error: url not defined!');
+		throw DOMException('Error: url not defined!');
+	}
+	self=this;
+	self._url=obj.url;
+	self._method=(obj.method!=void 0 && (obj.method instanceof String))?obj.method.toUpperCase():'GET';
+	self._xhr=new XMLHttpRequest();
+	self._data=(obj.data!=void 0  && data instanceof FormData)?obj.data:new FormData();
+	self._header=(obj.headers instanceof Object && Object.keys(obj.headers).length>0)?obj.headers:{};
+	self._beforeSend=(obj.beforeSend)?obj.beforeSend:null;
+	self._onError=(obj.beforeSend)?obj.beforeSend:null;
+	self._onSuccess=(obj.success)?obj.success:null;
+	console.log([this]);
+	if(self._method=='POST'){
+		if(Object.keys(obj.data).length>0 && obj.data instanceof Object && !(obj.data instanceof FormData)){
 			//for(let [name,value] of data){
 			//	f.append(name,value);
 			//}
 			//use this for IE 11- or something
-			for(var i in data){
-				if(data[i]!=null){
-					f.append(i,data[i]);
+			for(var i in obj.data){
+				if(obj.data[i]!=null){
+					self._data.append(i,obj.data[i]);
 				}
 			}
 		}
-	}else if(method=='GET'){
-		url+="?";
-		for(let [name,value] of data){
-			url+=name+"="+value+"&";
+	}else if(self._method=='GET'){
+		if(Object.keys(this._data).length>0){
+			url="";
+			for(let [name,value] of self._data){
+				url+=name+"="+value+"&";
+			}
+			url=url.slice(0,-1);
+			self._url+=((url.length>0)?((self._url.indexOf("?")>-1)?"?":"&")+url:'');
 		}
 	}
-
-	xhr.open(method,url);
-	xhr.setRequestHeader('Accept','application/json');
-	xhr.onreadystatechange=function(){
-		if(xhr.readyState != 4){
+	self._xhr.open(self._method,self._url);
+	if(Object.keys(self._header).length>0){
+		for(let [name,value] of self._header){
+			self._xhr.setRequestHeader(name,value);
+		}
+	}
+	self._xhr.onreadystatechange=function(){
+		if(self._xhr.readyState!=4){
 			return;
 		}
-		if(xhr.readyState==4){
-			if(xhr.status==200){
-				if(callback!=null){
-					if(typeof callback === 'function'){
-						return callback(xhr.responseText);
-					}else{
-						if(window[callback]!=void 0){
-							callback = window[callback];
-							return callback(xhr.responseText);
-						}
-					}
+		if(self._xhr.readyState==4){
+			if(self._xhr.status==200){
+				if(self._onSuccess!=null){
+					return self._onSuccess(self._xhr.responseText);
+				}
+			}else{
+				if(self._onError!=null){
+					return self._onError();
 				}
 			}
 		}
 	};
-	xhr.send(f);
+	if(self._beforeSend!=null){
+		self._beforeSend();
+	}
+	self._xhr.send(self._data);
 };
+ajax({url:'https://amironov.city-travel.ru/api/message/get'});
